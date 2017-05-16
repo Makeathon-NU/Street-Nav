ca3a2e4a2c5edb2e9803c4f32355d335f3706998
julied4 <julied4@gmail.com>
2017-05-16 13:12:56 -0500

    Add files via upload

---
+++ AddrSuffixTable.js
@@ -0,0 +1,65 @@
+﻿/**
+ * AddrSuffixTable.js
+ * Table of address abbreviations and their full names.
+ *
+ * Made for TOM Makeathon at Northwestern University
+ *
+ * @license MIT license
+ * @version 1.0
+ * @author  Daniel Bednarczyk, Darcy Green (Need Knower), Joe Cummings, Julie Davies, Megan Reid, Wong Song Wei
+ * @updated 2017-05-16
+ * @link    https://makeathon-nu.github.io/Street-Nav/
+ *
+ *
+ */
+
+var m_aryAddrSuffixTable = [
+["ave", "Avenue"],
+["av", "Avenue"],
+["bch", "Beach"],
+["bnd", "Bend"],
+["blf", "Bluff"],
+["blvd", "Boulevard"],
+["br", "Branch"],
+["cyn", "Canyon"],
+["ctr", "Center"],
+["cir", "Circle"],
+["clf", "Cliff"],
+["ct", "Court"],
+["xing", "Crossing"],
+["dr", "Drive"],
+["grv", "Grove"],
+["jct", "Junction"],
+["ln", "Lane"],
+["mt", "Mount"],
+["mtn", "Mountain"],
+["opas", "Overpass"],
+["pkwy", "Parkway"],
+["pl", "Place"],
+["plz", "Plaza"],
+["pt", "Point"],
+["prt", "Port"],
+["rdg", "Ridge"],
+["rd", "Road"],
+["spg", "Spring"],
+["sq", "Square"],
+["sta", "Station"],
+["st", "Street"],
+["ter", "Terrace"],
+["trwy", "Throughway"],
+["trl", "Trail"],
+["tunl", "Tunnel"],
+["tpke", "Turnpike"],
+["upas", "Underpass"],
+["vly", "Valley"],
+["vw", "View"],
+["vlg", "Village"],
+["n", "North"],
+["ne", "Northeast"],
+["nw", "Northwest"],
+["e", "East"],
+["w", "West"],
+["s", "South"],
+["sw", "Southwest"],
+["se", "Southeast"],
+];
